69b874f526ad7a793e2f1ded574b2140ed347ea5
Ethan Hu <ethanhu99@gmail.com>
2022-08-20 03:50:44 -0700

    update blank template

---
+++ src/components/experience/experience.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+
+export default class About extends Component {
+
+render() {
+  return (
+     <div> paste the content of the component translated to JSX </div>
+  )
+ }
+}
